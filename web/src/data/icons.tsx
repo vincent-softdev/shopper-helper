7252f64b1af145e78a12b6bd8ed85d94ac9161a8
davin-softdev <vinh.ly.softdev@gmail.com>
2023-08-17 18:16:23 +0930

    update the service ui

--- web/src/data/icons.tsx
+++ web/src/data/icons.tsx
@@ -4,7 +4,21 @@ type Stores = {
     Woolies: string
 }
 
+type Service = {url: string, name: string, detail: string}
+
+type Icons = {
+    ExtractText: Service,
+    Distance: Service,
+    GoodPrice: Service
+}
+
 export const storeIcons: Stores = {
     Coles: "https://www.coles.com.au/content/dam/coles/coles-app/icons/coles-app-Icon-coles-truck-470x282px.svg",
     Woolies: "https://play-lh.googleusercontent.com/BFup0y0U_i4Gr69xrhowoHqE--sMVS40o86EbsGE7hAkIKiVjL0_R_T7sPQAuMX6NA"
+}
+
+export const icons: Icons = {
+    ExtractText: {url: "https://miro.medium.com/v2/resize:fit:413/1*L5zVN_jPtO8CMqNYO6rlug.png", name: "Extract Data", detail: "Extract detail from Images",},
+    Distance: {url: "https://cdn-icons-png.flaticon.com/512/3710/3710274.png", name: "Minimise Distance", detail: "Show the best store base on selected products",},
+    GoodPrice: {url: "https://img.freepik.com/premium-vector/banner-i-recommend-is-red-with-thumb-up-vector-illustration_185004-402.jpg", name: "Best Price", detail: "Filter out the best price base on stores",}
 }
--- web/src/pages/home/services/services.tsx
+++ web/src/pages/home/services/services.tsx
@@ -1,8 +1,33 @@
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 import './services.css'
+import {icons} from '../../../data/icons'
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 const HomeContentServices = () => {
     return <div className="home-content-services__container">
-        this is home content services
+        <Grid className='container-body' container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {[icons.ExtractText, icons.Distance, icons.GoodPrice].map((_, index) => (
+                <Grid className='items-container' item xs={2} sm={4} md={4} key={index}>
+                    <Item title={_.detail}>
+                        <h1 className='service-name'>{_.name}</h1>
+                        <div className='service-items'>
+                            <img src={_.url} />
+                        </div>
+                        {/* <p className='service-detail'>{_.detail}</p> */}
+                    </Item>
+                </Grid>
+            ))}
+        </Grid>
     </div>
 }
 
